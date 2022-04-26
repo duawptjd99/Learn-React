a74c5acaa2e3da6c52127ecfa033ab75ff27ac3f
duawptjd99 <duawptjd99@naver.com>
2022-04-26 16:19:46 +0900

    Learn Styling for CSS/Sass/CSS Module/styled-components

---
+++ styling-react/src/App.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
+//import SassComponent from "./SassComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent/>
+      </div>
+    );
+  }
+}
+
+export default App;

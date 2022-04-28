eb727ea4078162395af1ffe454be36908ff75583
duawptjd99 <duawptjd99@naver.com>
2022-04-28 22:03:00 +0900

    일정관리 프로그램

---
+++ immer-tutorial/src/App.js
@@ -0,0 +1,21 @@
+import { useRef, useCallback, useState } from "react";
+
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: "", username: "" });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
+
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setForm({
+        ...form,
+        [name]: [value],
+      });
+    },
+    [form]
+  );
+};
